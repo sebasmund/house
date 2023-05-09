d6ef2a8df74b3c2d0eaded150897630708c66262
Miguel Angel <brnjamincarmine@gmail.com>
2023-05-09 11:11:16 -0500

    Agregación Imágenes y Botones
    Se agregaron a todas las habitaciones de la casa las imágenes con la luz prendida.

--- script.js
+++ script.js
@@ -13,6 +13,58 @@ function luzH1() {
   var h1l = document.querySelector('.h1l');
   h1l.classList.toggle('oculto');
 }
+function luzHp() {
+  var hpl = document.querySelector('.hpl');
+  hpl.classList.toggle('oculto');
+}
+function luzHnl() {
+  var hnl= document.querySelector('.hnl');
+  hnl.classList.toggle('oculto');
+}
+
+function luzComedor() {
+  var comedorl= document.querySelector('.comedorl');
+  comedorl.classList.toggle('oculto');
+
+}
+function luzBano1() {
+  var bano1l= document.querySelector('.baño1l');
+  bano1l.classList.toggle('oculto');
+  
+}
+function luzBano2() {
+  var bano2l= document.querySelector('.b2l');
+  bano2l.classList.toggle('oculto');
+  
+}
+
+function luzCocina() {
+  var cocinal= document.querySelector('.cocinal');
+  cocinal.classList.toggle('oculto');
+  
+}
+function luzGarage() {
+  var garagel= document.querySelector('.garagel');
+  garagel.classList.toggle('oculto');
+  
+}
+
+function luzPasillo1() {
+  var pasillocl= document.querySelector('.pasillocl');
+  pasillocl.classList.toggle('oculto');
+  
+}
+function luzPasillo2() {
+  var pasillopl= document.querySelector('.pasillopl');
+  pasillopl.classList.toggle('oculto');
+  
+}
+
+function luzSala() {
+  var salal= document.querySelector('.salal');
+  salal.classList.toggle('oculto');
+  
+}
 
 function mostrarOpciones() {
   var opciones = document.querySelector(".dropdown-content");
