79dbe5190f7adedaadb41bf5a99dde5d8c00315f
sebasmund <sebasmundaray@gmail.com>
2023-05-09 23:12:46 -0500

    Simplificación codigo

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+//encendido
 function luzSotano() {
   var sotanol = document.querySelector('.sotanol');
   sotanol.classList.toggle('oculto');
 }
 
-
 function luzEscaleras() {
   var escalerasl = document.querySelector('.escalerasl');
   escalerasl.classList.toggle('oculto');
 }
-
 
 function luzLavado() {
   var lavadol = document.querySelector('.lavadol');
@@ -19,10 +18,12 @@ function luzH1() {
   var h1l = document.querySelector('.h1l');
   h1l.classList.toggle('oculto');
 }
+
 function luzHp() {
   var hpl = document.querySelector('.hpl');
   hpl.classList.toggle('oculto');
 }
+
 function luzHnl() {
   var hnl= document.querySelector('.hnl');
   hnl.classList.toggle('oculto');
@@ -33,11 +34,13 @@ function luzComedor() {
   comedorl.classList.toggle('oculto');
 
 }
+
 function luzBano1() {
   var bano1l= document.querySelector('.baño1l');
   bano1l.classList.toggle('oculto');
   
 }
+
 function luzBano2() {
   var bano2l= document.querySelector('.b2l');
   bano2l.classList.toggle('oculto');
@@ -49,6 +52,7 @@ function luzCocina() {
   cocinal.classList.toggle('oculto');
   
 }
+
 function luzGarage() {
   var garagel= document.querySelector('.garagel');
   garagel.classList.toggle('oculto');
@@ -60,6 +64,7 @@ function luzPasillo1() {
   pasillocl.classList.toggle('oculto');
   
 }
+
 function luzPasillo2() {
   var pasillopl= document.querySelector('.pasillopl');
   pasillopl.classList.toggle('oculto');
@@ -71,56 +76,67 @@ function luzSala() {
   salal.classList.toggle('oculto');
   
 }
+
 function puertaSotano(){
   var psa= document.querySelector('.psa');
   psa.classList.toggle('oculto');
   
 }
+
 function puertaLavado(){
   var pcla= document.querySelector('.pcla');
   pcla.classList.toggle('oculto');
   
 }
+
 function VetanaBa(){
   var vb1a= document.querySelector('.vb1a');
   vb1a.classList.toggle('oculto');
   
 }
+
 function AbrirClo1(){
   var cp2l= document.querySelector('.cp2l');
   cp2l.classList.toggle('oculto');
   
 }
+
 function AbrirClo2(){
   var cp1l= document.querySelector('.cp1l');
   cp1l.classList.toggle('oculto');
   
 }
-function AbrirCloH1(){
+
+function AbrirClo1(){
   var c1a= document.querySelector('.c1a');
   c1a.classList.toggle('oculto');
   
 }
+
 function VentanaBa2(){
   var vb2a= document.querySelector('.vb2a');
   vb2a.classList.toggle('oculto');
   
 }
+
 function puertaH1(){
   var h1pa= document.querySelector('.h1pa');
   h1pa.classList.toggle('oculto');
   
 }
+
 function puertaHp(){
   var phpa= document.querySelector('.phpa');
   phpa.classList.toggle('oculto');
   
 }
+
 function VentanaNpa(){
   var npa= document.querySelector('.nva');
   npa.classList.toggle('oculto');
   
 }
+
 function AbrirVentanaHp(){
   var vhpa= document.querySelector('.vhpa');
   vhpa.classList.toggle('oculto');
@@ -132,31 +148,37 @@ function AbrirVentanaH1(){
 h1va.classList.toggle('oculto');
   
 }
+
 function puertaNpa(){
   var npa= document.querySelector('.npa');
   npa.classList.toggle('oculto');
   
 }
+
 function VentanaVa(){
   var va= document.querySelector('.va');
   va.classList.toggle('oculto');
   
 }
+
 function PuertaBa(){
   var pb1a= document.querySelector('.pb1a');
   pb1a.classList.toggle('oculto');
   
 }
+
 function VentanaVca(){
   var vca= document.querySelector('.vca');
   vca.classList.toggle('oculto');
   
 }
+
 function PuertaPga(){
   var pga= document.querySelector('.pga');
   pga.classList.toggle('oculto');
   
 }
+
 function PuertaPcga(){
   var pcga= document.querySelector('.pcga');
   pcga.classList.toggle('oculto');
@@ -168,126 +190,24 @@ function PuertaBa2(){
   pb2a.classList.toggle('oculto');
   
 }
+
 function VentanaSala(){
   var vsa= document.querySelector('.vsa');
   vsa.classList.toggle('oculto');
   
 }
+
 function PuertaPasillo2(){
   var ppa= document.querySelector('.ppa');
   ppa.classList.toggle('oculto');
   
 }
-function mostrarOpciones12() {
-  var opciones12 = document.querySelector(".dropdown-content12");
-  if (opciones12.style.display === "block") {
-    opciones12.style.display = "none";
-  } else {
-    opciones12.style.display = "block";
-  }
-}
-function mostrarOpciones11() {
-  var opciones11 = document.querySelector(".dropdown-content11");
-  if (opciones11.style.display === "block") {
-    opciones11.style.display = "none";
-  } else {
-    opciones11.style.display = "block";
-  }
-}
-function mostrarOpciones9() {
-  var opciones9 = document.querySelector(".dropdown-content9");
-  if (opciones9.style.display === "block") {
-    opciones9.style.display = "none";
-  } else {
-    opciones9.style.display = "block";
-  }
-}
-function mostrarOpciones10() {
-  var opciones10 = document.querySelector(".dropdown-content10");
-  if (opciones10.style.display === "block") {
-    opciones10.style.display = "none";
-  } else {
-    opciones10.style.display = "block";
-  }
-}
-function mostrarOpciones6() {
-  var opciones6 = document.querySelector(".dropdown-content6");
-  if (opciones6.style.display === "block") {
-    opciones6.style.display = "none";
-  } else {
-    opciones6.style.display = "block";
-  }
-}
-function mostrarOpciones7() {
-  var opciones7 = document.querySelector(".dropdown-content7");
-  if (opciones7.style.display === "block") {
-    opciones7.style.display = "none";
-  } else {
-    opciones7.style.display = "block";
-  }
-}
-function mostrarOpciones8() {
-  var opciones8 = document.querySelector(".dropdown-content8");
-  if (opciones8.style.display === "block") {
-    opciones8.style.display = "none";
-  } else {
-    opciones8.style.display = "block";
-  }
-}
 
-function mostrarOpciones5() {
-  var opciones5 = document.querySelector(".dropdown-content5");
-  if (opciones5.style.display === "block") {
-    opciones5.style.display = "none";
+function showDropdown(id) {
+  var dropdownMenu = document.getElementById(id);
+  if (dropdownMenu.style.display === "none") {
+    dropdownMenu.style.display = "block";
   } else {
-    opciones5.style.display = "block";
+    dropdownMenu.style.display = "none";
   }
-}
-
-function mostrarOpciones() {
-  var opciones = document.querySelector(".dropdown-content");
-  if (opciones.style.display === "block") {
-    opciones.style.display = "none";
-  } else {
-    opciones.style.display = "block";
-  }
-}
-
-function mostrarOpciones1() {
-  var opciones1 = document.querySelector(".dropdown-content1");
-  if (opciones1.style.display === "block") {
-    opciones1.style.display = "none";
-  } else {
-    opciones1.style.display = "block";
-  }
-}
-function mostrarOpciones2() {
-  var opciones2 = document.querySelector(".dropdown-content2");
-  if (opciones2.style.display === "block") {
-    opciones2.style.display = "none";
-  } else {
-    opciones2.style.display = "block";
-  }
-}
-function mostrarOpciones3() {
-  var opciones3 = document.querySelector(".dropdown-content3");
-  if (opciones3.style.display === "block") {
-    opciones3.style.display = "none";
-  } else {
-    opciones3.style.display = "block";
-  }
-}
-
-function mostrarOpciones4() {
-  var opciones4 = document.querySelector(".dropdown-content4");
-  if (opciones4.style.display === "block") {
-    opciones4.style.display = "none";
-  } else {
-    opciones4.style.display = "block";
-  }
-}
-/* Función para cerrar el menú desplegable */
-function cerrarOpciones() {
-  var opciones = document.querySelector(".dropdown-content");
-  opciones.style.display = "none";
 }
